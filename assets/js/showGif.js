7cfdc5b1cc7a758e6d256b27b18a67c0e8c00ab6
Nikita Maslov <118814780+NikitaMaslov93@users.noreply.github.com>
2024-07-11 17:41:36 +0300

    site upload

---
+++ assets/js/showGif.js
@@ -0,0 +1,9 @@
+function showGif() {
+  var modal = document.getElementById("gifModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("gifModal");
+  modal.style.display = "none";
+}
